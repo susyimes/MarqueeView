f2c86395cd1bd59e9068d32be3712c6b251c2d39
susyimes <705710652@qq.com>
2019-12-10 17:17:34 +0800

    clean ver

--- marqueeview/src/main/java/com/sunfusheng/marqueeview/MarqueeView.java
+++ marqueeview/src/main/java/com/sunfusheng/marqueeview/MarqueeView.java
@@ -52,9 +52,10 @@ public class MarqueeView<T> extends ViewFlipper {
     @AnimRes
     private int outAnimResId = R.anim.anim_top_out;
 
-    private int position;
+    //private int position;
     private List<T> messages = new ArrayList<>();
     private OnItemClickListener onItemClickListener;
+
 
     public MarqueeView(Context context) {
         this(context, null);
@@ -223,52 +224,53 @@ public class MarqueeView<T> extends ViewFlipper {
     private void start(final @AnimRes int inAnimResId, final @AnimRes int outAnimResID) {
         removeAllViews();
         clearAnimation();
+        createTextView();
         // 检测数据源
         if (messages == null || messages.isEmpty()) {
             throw new RuntimeException("The messages cannot be empty!");
         }
-        position = 0;
-        addView(createTextView(messages.get(position)));
+       // position = 0;
+       // addView(createTextView(messages.get(position)));
 
         if (messages.size() > 1) {
             setInAndOutAnimation(inAnimResId, outAnimResID);
             startFlipping();
         }
 
-        if (getInAnimation() != null) {
-            getInAnimation().setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-                    if (isAnimStart) {
-                        animation.cancel();
-                    }
-                    isAnimStart = true;
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    position++;
-                    if (position >= messages.size()) {
-                        position = 0;
-                    }
-                    View view = createTextView(messages.get(position));
-                    if (view.getParent() == null) {
-                        addView(view);
-                    }
-                    isAnimStart = false;
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-                }
-            });
-        }
+//        if (getInAnimation() != null) {
+//            getInAnimation().setAnimationListener(new Animation.AnimationListener() {
+//                @Override
+//                public void onAnimationStart(Animation animation) {
+//                    if (isAnimStart) {
+//                        animation.cancel();
+//                    }
+//                    isAnimStart = true;
+//                }
+//
+//                @Override
+//                public void onAnimationEnd(Animation animation) {
+////                    position++;
+////                    if (position >= messages.size()) {
+////                        position = 0;
+////                    }
+////                    View view = createTextView(messages.get(position));
+////                    if (view.getParent() == null) {
+////                        addView(view);
+////                    }
+//                    isAnimStart = false;
+//                }
+//
+//                @Override
+//                public void onAnimationRepeat(Animation animation) {
+//                }
+//            });
+//        }
     }
 
-    private TextView createTextView(T marqueeItem) {
-        TextView textView = (TextView) getChildAt((getDisplayedChild() + 1) % 3);
-        if (textView == null) {
-            textView = new TextView(getContext());
+    private void createTextView() {
+//        (getDisplayedChild() + 1) % 3
+        for (T message : messages){
+          TextView  textView = new TextView(getContext());
             textView.setGravity(gravity | Gravity.CENTER_VERTICAL);
             textView.setTextColor(textColor);
             textView.setTextSize(textSize);
@@ -289,17 +291,49 @@ public class MarqueeView<T> extends ViewFlipper {
                     }
                 }
             });
-            CharSequence message = "";
-            if (marqueeItem instanceof CharSequence) {
-                message = (CharSequence) marqueeItem;
-            } else if (marqueeItem instanceof IMarqueeItem) {
-                message = ((IMarqueeItem) marqueeItem).marqueeMessage();
-            }
-            textView.setText(message);
-            textView.setTag(position);
-        }
 
-        return textView;
+            if (message instanceof CharSequence) {
+                textView.setText((CharSequence) message);
+            } else if (message instanceof IMarqueeItem) {
+                textView.setText(((IMarqueeItem) message).marqueeMessage());
+            }
+            //textView.setTag(position);
+            addView(textView);
+        }
+//        TextView textView = (TextView) getChildAt(position);
+//        if (textView == null) {
+//            textView = new TextView(getContext());
+//            textView.setGravity(gravity | Gravity.CENTER_VERTICAL);
+//            textView.setTextColor(textColor);
+//            textView.setTextSize(textSize);
+//            textView.setIncludeFontPadding(true);
+//            textView.setSingleLine(singleLine);
+//            if (singleLine) {
+//                textView.setMaxLines(1);
+//                textView.setEllipsize(TextUtils.TruncateAt.END);
+//            }
+//            if (typeface != null) {
+//                textView.setTypeface(typeface);
+//            }
+//            textView.setOnClickListener(new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (onItemClickListener != null) {
+//                        onItemClickListener.onItemClick(getPosition(), (TextView) v);
+//                    }
+//                }
+//            });
+//            CharSequence message = "";
+//            if (marqueeItem instanceof CharSequence) {
+//                message = (CharSequence) marqueeItem;
+//            } else if (marqueeItem instanceof IMarqueeItem) {
+//                message = ((IMarqueeItem) marqueeItem).marqueeMessage();
+//            }
+//            textView.setText(message);
+//            textView.setTag(position);
+//        }
+
+        //return textView;
     }
 
     public int getPosition() {
